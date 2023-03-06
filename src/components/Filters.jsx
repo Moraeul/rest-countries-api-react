bc6ab3340443ff4e109178021a8d55419c5598de
Vinicius Rasquinha <vinicius.rasquinha@outlook.com>
2023-03-06 17:08:24 -0300

    style: Filters search input filed

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
 import ListBox from './ListBox';
 
 export default function Filters() {
   return (
     <form className='my-8 flex-row space-y-8'>
-      <input
-        type='search'
-        className='py-2 px-4 w-full rounded-md shadow-md shadow-gray-300 dark:bg-gray-700 dark:shadow-gray-900'
-      />
+      <label htmlFor='default-search' className='sr-only text-sm'>
+        Search
+      </label>
+
+      <div className='relative'>
+        <div className='pointer-events-none absolute inset-y-0 flex items-center pl-6'>
+          <AiOutlineSearch className='text-xl text-gray-400 dark:text-gray-100' />
+        </div>
+
+        <input
+          id='default-search'
+          type='search'
+          placeholder='Search for a country...'
+          className='py-4 pl-16 text-sm pr-6 w-full rounded-md shadow-md shadow-gray-300 dark:bg-gray-700 dark:shadow-gray-900'
+        />
+      </div>
 
       <ListBox />
     </form>
