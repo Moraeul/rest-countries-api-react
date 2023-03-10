cd6089116adfee129e105cf2f7bfa2fbd5b960f9
Vinicius Rasquinha <vinicius.rasquinha@outlook.com>
2023-03-10 16:34:22 -0300

    style: Details.jsx responsiveness

--- src/components/DetailsInfo.jsx
+++ src/components/DetailsInfo.jsx
@@ -23,57 +23,61 @@ export default function CountryDetailsInfo({ currentCountry, countries }) {
 
   const formattedPopulation = population.toLocaleString('en-US');
   const formattedLanguages = Object.values(languages).join(', ');
+
   const formattedCurrencies = Object.values(currencies).map(
     (curr) => curr.name,
   );
 
   return (
-    <>
-      <img src={svg} alt={alt} />
+    <div className='xl:flex xl:space-x-20'>
+      <img src={svg} alt={alt} className='xl:max-w-[50%]' />
 
       <div className='leading-7 text-sm dark:text-gray-200'>
         <h1 className='font-extrabold text-xl my-6'>{name}</h1>
-        <div>
-          <p>
-            <span className='font-semibold'>Native Name: </span>
-            {altSpellings[1]}
-          </p>
-          <p>
-            <span className='font-semibold'>Population: </span>
-            {formattedPopulation}
-          </p>
-          <p>
-            <span className='font-semibold'>Region: </span>
-            {region}
-          </p>
-          <p>
-            <span className='font-semibold'>Sub Region: </span>
-            {subregion}
-          </p>
-          <p>
-            <span className='font-semibold'>Capital: </span>
-            {capital}
-          </p>
+
+        <div className='md:flex md:space-x-20 mb-6'>
+          <div>
+            <p>
+              <span className='font-semibold'>Native Name: </span>
+              {altSpellings[1]}
+            </p>
+            <p>
+              <span className='font-semibold'>Population: </span>
+              {formattedPopulation}
+            </p>
+            <p>
+              <span className='font-semibold'>Region: </span>
+              {region}
+            </p>
+            <p>
+              <span className='font-semibold'>Sub Region: </span>
+              {subregion}
+            </p>
+            <p>
+              <span className='font-semibold'>Capital: </span>
+              {capital}
+            </p>
+          </div>
+
+          <div className='my-6 md:m-0'>
+            <p>
+              <span className='font-semibold'>Top Level Domain: </span>
+              {tld.join(', ')}
+            </p>
+            <p>
+              <span className='font-semibold'>Currencies: </span>
+              {formattedCurrencies}
+            </p>
+            <p>
+              <span className='font-semibold'>Languages: </span>
+              {formattedLanguages}
+            </p>
+          </div>
         </div>
 
-        <div className='my-6'>
-          <p>
-            <span className='font-semibold'>Top Level Domain: </span>
-            {tld.join(', ')}
-          </p>
-          <p>
-            <span className='font-semibold'>Currencies: </span>
-            {formattedCurrencies}
-          </p>
-          <p>
-            <span className='font-semibold'>Languages: </span>
-            {formattedLanguages}
-          </p>
-        </div>
-
         <div>
-          <h1 className='font-semibold mb-1 text-lg'>Border Countries:</h1>
-          <div className='flex-wrap space-y-3'>
+          <h1 className='font-semibold mb-2 text-lg'>Border Countries:</h1>
+          <div className='flex-wrap'>
             {borders
               ? borders.map((cca3) => {
                   const borderFound = findBorders(cca3);
@@ -81,7 +85,7 @@ export default function CountryDetailsInfo({ currentCountry, countries }) {
                     <Link
                       to={`/country/${borderFound.name.common}`}
                       key={cca3}
-                      className='shadow-md inline-block rounded-md mr-3 shadow-gray-300 py-1 px-4 bg-white dark:bg-gray-700 dark:shadow-sm dark:shadow-gray-900'
+                      className='shadow-md mb-3 inline-block rounded-md mr-3 shadow-gray-300 py-1 px-4 bg-white dark:bg-gray-700 dark:shadow-sm dark:shadow-gray-900'
                     >
                       {borderFound.name.common}
                     </Link>
@@ -91,6 +95,6 @@ export default function CountryDetailsInfo({ currentCountry, countries }) {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
